b14744ec9dcaedca621c97ab2964626e5348ddc8
Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>
2024-09-16 11:03:42 +0200

    install our platform in devcontainer
    Signed-off-by: Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,6 +5,6 @@ echo "$(date): Running post-create.sh" >> ~/.status.log
 
 # this runs in background after UI is available
 
-k3d cluster create --config .devcontainer/k3d.yaml --wait --verbose 2>&1 | tee -a ~/.status.log
+# k3d cluster create --config .devcontainer/k3d.yaml --wait --verbose 2>&1 | tee -a ~/.status.log
 
 echo "$(date): Finished post-create.sh" >> ~/.status.log
--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -5,24 +5,23 @@ echo "$(date): Running post-start.sh" >> ~/.status.log
 
 # this runs in background each time the container starts
 
-# Ensure kubeconfig is set up. 
-k3d kubeconfig merge dev --kubeconfig-merge-default
+export GITHUB_CLIENTSECRET=dummy
+export GITHUB_CLIENTID=dummy
+export GITHUB_TOKEN=dummy
+export GITHUB_APPSET_TOKEN=dummy
+export TARGET_TYPE=KIND-DELIVERY
+export CURRENT_BRANCH=feat/devcontainer
+export CREATE_K3D_CLUSTER=true
 
-kubectl apply -k argocd 2>&1 | tee -a ~/.status.log
+# install mkcert
+curl -JLO "https://dl.filippo.io/mkcert/latest?for=linux/amd64"
+chmod +x mkcert-v*-linux-amd64
+sudo mv mkcert-v*-linux-amd64 /usr/local/bin/mkcert
 
-kubectl wait deployment -n argocd --all --for=condition=Available=True --timeout=90s 2>&1 | tee -a ~/.status.log
-ARGOCD_ADMIN_PASSWORD="$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)"
-echo "${ARGOCD_ADMIN_PASSWORD}" > ~/argo-cd-admin-password.txt
+# kind create cluster --name devcontainer-cluster --config=.github/kind-config.yaml
 
-kubectl apply -f apps.yaml 2>&1 | tee -a ~/.status.log
+k3d cluster list
 
-argocd login \
-  "localhost:31443" \
-  --username admin \
-  --password ${ARGOCD_ADMIN_PASSWORD} \
-  --grpc-web \
-  --insecure 2>&1 | tee -a ~/.status.log
-
-echo "Argo CD admin password: ${ARGOCD_ADMIN_PASSWORD}"
+curl -L https://raw.githubusercontent.com/suxess-it/sx-cnp-oss/${CURRENT_BRANCH}/install-platform.sh | bash
 
 echo "$(date): Finished post-start.sh" >> ~/.status.log
