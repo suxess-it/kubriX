de6bb9b9349c3be32d7bb1c50f4b39a29d68e391
Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>
2024-09-16 22:17:46 +0200

    add other scenarios and fix port information
    Signed-off-by: Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>

---
+++ .devcontainer/portal/post-start.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e  # Exit on non-zero exit code from commands
+
+echo "$(date): Running post-start.sh" >> ~/.status.log
+
+# this runs in background each time the container starts
+
+export GITHUB_CLIENTSECRET=dummy
+export GITHUB_CLIENTID=dummy
+export GITHUB_TOKEN=dummy
+export GITHUB_APPSET_TOKEN=dummy
+export TARGET_TYPE=KIND-PORTAL
+export CURRENT_BRANCH=$( git rev-parse --abbrev-ref HEAD )
+export CREATE_K3D_CLUSTER=true
+
+# install mkcert
+curl -JLO "https://dl.filippo.io/mkcert/latest?for=linux/amd64"
+chmod +x mkcert-v*-linux-amd64
+sudo mv mkcert-v*-linux-amd64 /usr/local/bin/mkcert
+
+# kind create cluster --name devcontainer-cluster --config=.github/kind-config.yaml
+
+k3d cluster list
+
+# Ensure kubeconfig is set up. 
+# k3d kubeconfig merge dev --kubeconfig-merge-default
+
+./install-platform.sh
+
+# forward argocd and kargo so it gets also exposed in github codespace
+nohup kubectl -n argocd port-forward svc/sx-argocd-server 6688:80 &
+nohup kubectl -n kargo port-forward svc/sx-backstage 6691:80 &
+
+argocd_password=$( kubectl get secret -n argocd argocd-initial-admin-secret -o=jsonpath='{.data.password}' | base64 -d )
+
+echo "kubrix delivery is set up sucessfully."
+echo "ArgoCD url: https://${CODESPACE_NAME}-6688.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
+echo "ArgoCD user: admin"
+echo "ArgoCD password: ${argocd_password}"
+echo ""
+echo "Backstage url: https://${CODESPACE_NAME}-6689.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
+
+echo "$(date): Finished post-start.sh" >> ~/.status.log
