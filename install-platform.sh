e12a05f315a986034e828a980c86281dca78cba2
Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>
2024-10-02 19:36:48 +0000

    logic for keycloak and github auth in codespace
    Signed-off-by: Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>

--- install-platform.sh
+++ install-platform.sh
@@ -223,9 +223,10 @@ echo "adding special configuration for sx-backstage"
   # in github codespace we need additional environment variables to overwrite app-config.yaml
   if [ ${CODESPACES} ]; then
     KEYCLOAK_CODESPACES=""
+    GITHUB_CODESPACES="true"
+    BACKSTAGE_CODESPACE_URL="https://${CODESPACE_NAME}-6691.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
   fi
   if [ ${KEYCLOAK_CODESPACES} ]; then
-    BACKSTAGE_CODESPACE_URL="https://${CODESPACE_NAME}-6691.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
     kubectl create secret generic -n backstage manual-secret \
       --from-literal=GITHUB_CLIENTSECRET=${KUBRIX_GITHUB_CLIENTSECRET} \
       --from-literal=GITHUB_CLIENTID=${KUBRIX_GITHUB_CLIENTID} \
@@ -245,6 +246,20 @@ echo "adding special configuration for sx-backstage"
       --from-literal=APP_CONFIG_catalog_providers_keycloakOrg_default_realm=sx-cnp-oss-codespaces \
       --from-literal=APP_CONFIG_catalog_providers_keycloakOrg_default_clientId=backstage-codespaces \
       --from-literal=APP_CONFIG_catalog_providers_keycloakOrg_default_clientSecret=demosecret
+
+  elif [ ${GITHUB_CODESPACES} ]; then
+    kubectl create secret generic -n backstage manual-secret \
+    --from-literal=GITHUB_CLIENTSECRET=${KUBRIX_GITHUB_CLIENTSECRET} \
+    --from-literal=GITHUB_CLIENTID=${KUBRIX_GITHUB_CLIENTID} \
+    --from-literal=GITHUB_ORG=${GITHUB_ORG} \
+    --from-literal=GITHUB_TOKEN=${KUBRIX_GITHUB_TOKEN} \
+    --from-literal=K8S_SA_TOKEN=${K8S_SA_TOKEN} \
+    --from-literal=ARGOCD_AUTH_TOKEN=${ARGOCD_AUTH_TOKEN} \
+    --from-literal=GRAFANA_TOKEN=${GRAFANA_TOKEN} \
+    --from-literal=APP_CONFIG_app_baseUrl=${BACKSTAGE_CODESPACE_URL} \
+    --from-literal=APP_CONFIG_backend_baseUrl=${BACKSTAGE_CODESPACE_URL} \
+    --from-literal=APP_CONFIG_backend_cors_origin=${BACKSTAGE_CODESPACE_URL} \
+    --from-literal=APP_CONFIG_auth_provider_github_development_callbackUrl=${BACKSTAGE_CODESPACE_URL}/api/auth/github/handler/frame
 
   else
     kubectl create secret generic -n backstage manual-secret \
