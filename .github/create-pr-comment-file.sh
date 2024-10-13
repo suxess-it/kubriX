b3ab687101da2bd9defcbe56019b14b4d62d75e1
Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>
2024-10-13 09:26:22 +0200

    reduce additional files to prevent max size

--- .github/create-pr-comment-file.sh
+++ .github/create-pr-comment-file.sh
@@ -25,7 +25,7 @@ for env in pr target; do
   cd -
 done
 diff -U 4 -r out-default-values/target out-default-values/pr > out/default-values-diff.txt || true
-diff -U 4 -r out/target out/pr > out/diff.txt || true
+diff -U 2 -r out/target out/pr > out/diff.txt || true
 csplit -f comment-files/comment out/diff.txt --elide-empty-files /^diff\ \-U/ '{*}'
 
 comment_files_csplit=$( find comment-files -type f | sort )
