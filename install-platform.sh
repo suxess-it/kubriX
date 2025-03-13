28b0c87bf015a8ca025e14ed33feaf03d6cc0530
phac008 <philipp.achmueller@suxess-it.com>
2025-03-13 11:54:22 +0100

    feat(keycloak): adapt keycloak dependency to vault

--- install-platform.sh
+++ install-platform.sh
@@ -221,7 +221,7 @@ fi
 
 if [[ "${KUBRIX_TARGET_TYPE}" =~ ^KIND.* ]] ; then
   # create mkcert certs in alle namespaces with ingress
-  for namespace in backstage kargo grafana argocd keycloak komoplane kubecost falco minio velero velero-ui vault; do
+  for namespace in backstage kargo grafana cnpg argocd komoplane kubecost falco minio velero velero-ui vault; do
     kubectl create namespace ${namespace}
     mkcert -cert-file ${namespace}-cert.pem -key-file ${namespace}-key.pem ${namespace}-127-0-0-1.nip.io
     # kargo needs a special secret name according to its helm chart
@@ -258,6 +258,10 @@ if [[ "${KUBRIX_TARGET_TYPE}" =~ ^KIND.* ]] ; then
     # and install nginx ingress-controller
     echo "installing nginx ingress controller in KinD"
     kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+
+    # create mkcert-issuer
+    kubectl create namespace cert-manager
+    kubectl create secret tls mkcert-ca-key-pair --key "$(mkcert -CAROOT)"/rootCA-key.pem --cert "$(mkcert -CAROOT)"/rootCA.pem -n cert-manager
 
     # vault oidc case
     echo "create a root ca and patch ingress-nginx-controller for vault oidc"
@@ -329,14 +333,54 @@ wait_until_apps_synced_healthy "${argocd_apps_without_individual}" "Synced" "Hea
 # apply argocd-secret to set a secretKey
 kubectl apply -f platform-apps/charts/argocd/manual-secret/argocd-secret.yaml
 
-# if kargo is part of this stack, upload token to vault
+# if vault is part of this stack, upload token to vault
 if [[ $( echo $argocd_apps | grep sx-vault ) ]] ; then
   echo "adding secrets in vault for sx-kargo and sx-team-onboarding ..."
   export VAULT_HOSTNAME=$(kubectl get ingress -o jsonpath='{.items[*].spec.rules[*].host}' -n vault)
-  curl -k --header "X-Vault-Token:$(kubectl get secret -n vault vault-init -o=jsonpath='{.data.root_token}'  | base64 -d)" --request POST --data "{\"data\": {\"KUBRIX_ARGOCD_APPSET_TOKEN\": \"${KUBRIX_ARGOCD_APPSET_TOKEN}\", \"KUBRIX_KARGO_GIT_PASSWORD\": \"${KUBRIX_KARGO_GIT_PASSWORD}\", \"KUBRIX_KARGO_GIT_USERNAME\": \"${KUBRIX_KARGO_GIT_USERNAME}\"}}" https://${VAULT_HOSTNAME}/v1/kubrix-kv/data/demo/delivery
+  export VAULT_TOKEN=$(kubectl get secret -n vault vault-init -o=jsonpath='{.data.root_token}'  | base64 -d)
+  curl -k --header "X-Vault-Token:$VAULT_TOKEN" --request POST --data "{\"data\": {\"KUBRIX_ARGOCD_APPSET_TOKEN\": \"${KUBRIX_ARGOCD_APPSET_TOKEN}\", \"KUBRIX_KARGO_GIT_PASSWORD\": \"${KUBRIX_KARGO_GIT_PASSWORD}\", \"KUBRIX_KARGO_GIT_USERNAME\": \"${KUBRIX_KARGO_GIT_USERNAME}\"}}" https://${VAULT_HOSTNAME}/v1/kubrix-kv/data/demo/delivery
   sleep 10
-fi
+  
+  if [[ "${KUBRIX_TARGET_TYPE}" =~ ^KIND.* ]] ; then
+  # due to issue #405 this step is needed for kind clusters
+    export OIDC_CONFIG=$(curl -s --header "X-Vault-Token: $VAULT_TOKEN" --request GET https://${VAULT_HOSTNAME}/v1/auth/oidc/config)
+    export VAULT_CLIENTSECRET=$(kubectl get secret -n keycloak keycloak-client-credentials -o=jsonpath='{.data.vault}'  | base64 -d)
+    export KEYCLOAK_HOSTNAME=$(kubectl get ingress -o jsonpath='{.items[*].spec.rules[*].host}' -n keycloak)
 
+    if [[ "$(echo "$OIDC_CONFIG" | jq -r '.errors')" != "null" ]]; then
+    set -x
+        echo "Setting up OIDC auth method"
+        curl -s --header "X-Vault-Token: $VAULT_TOKEN" --request POST --data '{"type": "oidc"}' https://${VAULT_HOSTNAME}/v1/sys/auth/oidc
+        echo "configure OIDC auth method"
+        curl -s --header "X-Vault-Token: $VAULT_TOKEN" --request POST --data '{"oidc_discovery_url": "https://'${KEYCLOAK_HOSTNAME}'/realms/kubrix", "oidc_client_id": "vault", "oidc_client_secret": "'$VAULT_CLIENTSECRET'", "default_role": "default", "oidc_discovery_ca_pem": "@/vault/userconfig/vault-ca/ca.crt"}' https://${VAULT_HOSTNAME}/v1/auth/oidc/config
+    fi
+  fi
+  # due to issue #422 this step is needed for all clusters
+    GROUP_ALIAS_LIST=$(curl -s --header "X-Vault-Token: $VAULT_TOKEN" --request LIST https://${VAULT_HOSTNAME}/v1/identity/group-alias/id)
+    if [ -z "$(echo "$GROUP_ALIAS_LIST" | jq -r '.data.keys | length')" ] || [ "$(echo "$GROUP_ALIAS_LIST" | jq -r '.data.keys | length')" -eq 0 ]; then
+        echo "No group aliases found. Setting up group aliases..."
+        # Get the list of identity groups
+        GROUP_LIST=$(curl -s --header "X-Vault-Token: $VAULT_TOKEN" --request LIST https://${VAULT_HOSTNAME}/v1/identity/group/name)
+        # Get OIDC accessor
+        ACC=$(curl -s --header "X-Vault-Token: $VAULT_TOKEN" --request GET https://${VAULT_HOSTNAME}/v1/sys/auth | jq -r '.["oidc/"].accessor')
+        echo "OIDC Accessor: $ACC"
+        # Iterate over groups and create group aliases
+        echo "$GROUP_LIST" | jq -r '.data.keys[]' | while read -r GROUP_NAME; do
+            echo "Processing group: $GROUP_NAME"
+            # Get Group ID
+            GROUP_ID=$(curl -s --header "X-Vault-Token: $VAULT_TOKEN" --request GET https://${VAULT_HOSTNAME}/v1/identity/group/name/$GROUP_NAME | jq -r '.data.id')
+            if [ -n "$GROUP_ID" ] && [ "$GROUP_ID" != "null" ]; then
+                # Create Group Alias
+                RESPONSE=$(curl -s --header "X-Vault-Token: $VAULT_TOKEN" --request POST --data '{"name": "'$GROUP_NAME'", "mount_accessor": "'$ACC'", "canonical_id": "'$GROUP_ID'"}' https://${VAULT_HOSTNAME}/v1/identity/group-alias)
+                echo "Group alias created for $GROUP_NAME: $RESPONSE"
+            else
+                echo "Group ID not found for $GROUP_NAME, skipping..."
+            fi
+        done
+    fi
+ fi
+
+# if kargo is part of this stack, upload token to vault
 if [[ $( echo $argocd_apps | grep sx-kargo ) ]] ; then
   kubectl delete ExternalSecret github-creds -n kargo
   # check if kargo is synced and healthy for 5 minutes
