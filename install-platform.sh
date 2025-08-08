d993f57a687bda1f153ded34bcc72d3ca1a6eaea
Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>
2025-08-08 22:56:23 +0200

    fix(installer): add argocd alias in coredns for kind installations

--- install-platform.sh
+++ install-platform.sh
@@ -344,6 +344,7 @@ if [[ "${KUBRIX_TARGET_TYPE}" =~ ^KIND.* || "${KUBRIX_CLUSTER_TYPE}" == "KIND" ]
     print;
     print "        rewrite name keycloak.127-0-0-1.nip.io ingress-nginx-controller.ingress-nginx.svc.cluster.local";
     print "        rewrite name grafana.127-0-0-1.nip.io ingress-nginx-controller.ingress-nginx.svc.cluster.local";
+    print "        rewrite name argocd.127-0-0-1.nip.io ingress-nginx-controller.ingress-nginx.svc.cluster.local";
     next
 }
 { print }
