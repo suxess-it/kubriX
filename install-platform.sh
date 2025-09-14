59ff6b1a37fb12d58efc878409f1512ba20c9304
Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>
2025-09-14 10:02:07 +0200

    Add k8sgpt tool check and analysis command

--- install-platform.sh
+++ install-platform.sh
@@ -77,7 +77,8 @@ check_prereqs() {
   check_tool kubectl "kubectl version --client=true"
   check_tool helm "helm version"
   check_tool curl "curl -V | head -1"
-
+  check_tool k8sgpt "k8sgpt version"
+  
   if [[ "${KUBRIX_TARGET_TYPE}" =~ ^KIND.* || "${KUBRIX_CLUSTER_TYPE}" == "KIND" ]] ; then
     check_tool mkcert "mkcert --version"
   fi
@@ -428,6 +429,8 @@ analyze_all_unhealthy_apps() {
       fi
     fi
   done
+  echo "==== k8sgpt analyze ===="
+  k8sgpt analyze
 }
 
 analyze_app() {
