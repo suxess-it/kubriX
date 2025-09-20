e08f879ef950312e7868af4c9477d7dc0382e4e9
Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>
2025-09-20 23:20:49 +0200

    Add rewrite rule for backstage in CoreDNS

--- install-platform.sh
+++ install-platform.sh
@@ -560,6 +560,7 @@ if [[ "${KUBRIX_TARGET_TYPE}" =~ ^KIND.* || "${KUBRIX_CLUSTER_TYPE}" == "KIND" ]
     print "        rewrite name grafana.127-0-0-1.nip.io ingress-nginx-controller.ingress-nginx.svc.cluster.local";
     print "        rewrite name argocd.127-0-0-1.nip.io ingress-nginx-controller.ingress-nginx.svc.cluster.local";
     print "        rewrite name vault.127-0-0-1.nip.io ingress-nginx-controller.ingress-nginx.svc.cluster.local";
+    print "        rewrite name backstage.127-0-0-1.nip.io ingress-nginx-controller.ingress-nginx.svc.cluster.local";
     next
 }
 { print }
