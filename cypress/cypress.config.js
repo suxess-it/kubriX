480609bfebc1771e3a347957416feb40066c31c4
Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>
2025-09-30 08:32:11 +0200

    Update baseUrl in Cypress configuration

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://backstage.demo.kubrix.cloud/",
-    supportFile: false
+    baseUrl: "https://backstage.127-0-0-1.nip.io/",
+    supportFile: false,
   },
+  video: true,
 });
