00379565b0242406f554e5457bce89d3757a0eea
Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>
2025-09-30 08:33:00 +0200

    Update Backstage URL in e2e test

--- cypress/cypress/e2e/backstage.cy.js
+++ cypress/cypress/e2e/backstage.cy.js
@@ -1,6 +1,6 @@
 describe("backstage", () => {
   it("tests backstage", () => {
-    cy.visit("https://backstage.demo.kubrix.cloud/");
+    cy.visit("https://backstage.127-0-0-1.nip.io/");
     cy.contains('Explore').click();
     //cy.get("div.jss4-2505 > div:nth-of-type(2) span.MuiButton-label-2578").click();
   });
