d4acc48ac23e2459666332dcc12f0d41b9ae2b4f
Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>
2025-09-30 15:53:30 +0200

    Update backstage.cy.js

--- cypress/cypress/e2e/portal/backstage.cy.js
+++ cypress/cypress/e2e/portal/backstage.cy.js
@@ -1,7 +1,5 @@
 describe("backstage", () => {
   it("tests backstage", () => {
     cy.visit("https://backstage.127-0-0-1.nip.io/");
-    cy.contains('Explore').click();
-    //cy.get("div.jss4-2505 > div:nth-of-type(2) span.MuiButton-label-2578").click();
   });
 });
