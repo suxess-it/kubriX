fd4e05e72084ca3a1e0e7081783778d52323636c
Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>
2025-09-30 09:21:55 +0200

    add tests for every stack
    Signed-off-by: Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>

---
+++ cypress/cypress/e2e/delivery/argocd.cy.js
@@ -0,0 +1,6 @@
+describe("argocd", () => {
+  it("tests argocd", () => {
+    cy.visit("https://argocd.127-0-0-1.nip.io/");
+    cy.contains('Explore').click();
+  });
+});
---
+++ cypress/cypress/e2e/observability/grafana.cy.js
@@ -0,0 +1,7 @@
+describe("grafana", () => {
+  it("tests grafana", () => {
+    cy.visit("https://grafana.127-0-0-1.nip.io/");
+    cy.contains('Explore').click();
+    //cy.get("div.jss4-2505 > div:nth-of-type(2) span.MuiButton-label-2578").click();
+  });
+});
