fbd05a52445a9527a0920734887ba1ba48ab6f34
Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>
2025-10-01 20:44:37 +0200

    Comment out ignoreHTTPSErrors in playwright.config.js
    Comment out the ignoreHTTPSErrors option in Playwright config.

--- e2e-tests/playwright/playwright.config.js
+++ e2e-tests/playwright/playwright.config.js
@@ -41,7 +41,7 @@ module.exports = defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     video: 'on',
-    ignoreHTTPSErrors: true,
+    // ignoreHTTPSErrors: true,
   },
 
   /* Configure projects for major browsers */
