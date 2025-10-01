edc4d2267ffda8af8244de0bfe4c446e15515e8f
Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>
2025-10-01 19:21:07 +0200

    Update page title in smoke test

--- e2e-tests/playwright/portal/portal.spec.js
+++ e2e-tests/playwright/portal/portal.spec.js
@@ -4,5 +4,5 @@ const { test, expect } = require("@playwright/test");
 test("Smoke 1 - has title", async ({ page }) => {
   await page.goto("https://backstage.127-0-0-1.nip.io/");
 
-  await expect(page).toHaveTitle(/Testkube/);
+  await expect(page).toHaveTitle(/kubriX OSS/);
 });
