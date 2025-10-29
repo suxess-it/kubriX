4b3bd9e10139a22d3e39e09e51c3f6a7207c1273
Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>
2025-10-29 21:52:36 +0100

    Update createsecret.sh

--- .secrets/createsecret.sh
+++ .secrets/createsecret.sh
@@ -145,7 +145,7 @@ EOF
           printf "  %s: %s\n" "$KEY" "$VALUE" >> "$TMPDIR/$SECRETFILE"
         fi
         if [[ "$HASHED" == "hashed" ]]; then
-          printf " %s: %s\n" "$KEY_hashed" "$(htpasswd -bnBC 10 "" ${pass} | tr -d ':\n')" >> "$TMPDIR/$SECRETFILE"
+          printf " %s_hashed: %s\n" "${KEY}" "$(htpasswd -bnBC 10 "" ${pass} | tr -d ':\n')" >> "$TMPDIR/$SECRETFILE"
         fi
     # Add data entry in PushSecret   
     cat <<EOF >> "$TMPDIR/push$SECRETFILE"
