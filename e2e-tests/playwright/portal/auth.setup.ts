c2fca3ddbf63ad22dc1273e5241e57c4fd0f4a2e
Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>
2025-11-14 12:30:45 +0100

    update
    Signed-off-by: Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>

--- e2e-tests/playwright/portal/auth.setup.ts
+++ e2e-tests/playwright/portal/auth.setup.ts
@@ -12,23 +12,14 @@ test('authenticate via GitHub and save state', async ({ page, context }) => {
     context.waitForEvent('page'),
   ]);
 
-  // Wait until the popup has loaded GitHub login UI
-  await popup.waitForLoadState();
 
-  await page.getByLabel('Username or email address').fill(process.env.E2E_TEST_GH_USERNAME!);
-  await page.getByLabel('Password').fill(process.env.E2E_TEST_GH_PASSWORD!);
-  await page.getByRole('button', { name: 'Sign in' }).click();
+  const page1Promise = page.waitForEvent('popup');
+  await page.getByRole('listitem').filter({ hasText: 'GitHubSign in using' }).getByRole('button').click();
+  const page1 = await page1Promise;
+  await page1.getByRole('textbox', { name: 'Username or email address' }).click();
+  await page1.getByRole('textbox', { name: 'Username or email address' }).fill(process.env.E2E_TEST_GH_USERNAME!);
+  await page1.getByRole('textbox', { name: 'Password' }).click();
+  await page1.getByRole('textbox', { name: 'Password' }).fill(process.env.E2E_TEST_GH_PASSWORD!);
+  await page1.getByRole('button', { name: 'Sign in' }).click();
 
-  const authorizeButton = page.getByRole('button', { name: /authorize/i });
-  if (await authorizeButton.isVisible().catch(() => false)) {
-    await authorizeButton.click();
-  }
-
-  // Wait until you’re clearly logged in
-  await page.waitForURL(/backstage.127-0-0-1.nip.io/);
-  // maybe wait for some Backstage element that only shows when logged in
-
-  const storagePath = path.join('playwright', '.auth', 'backstage.json');
-  fs.mkdirSync(path.dirname(storagePath), { recursive: true });
-  await page.context().storageState({ path: storagePath });
 });
