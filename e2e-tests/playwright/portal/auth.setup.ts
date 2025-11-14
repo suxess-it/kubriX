b2bc440a0a72a460c98573a14236587c49d59953
Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>
2025-11-14 08:03:02 +0100

    Update auth.setup.ts

--- e2e-tests/playwright/portal/auth.setup.ts
+++ e2e-tests/playwright/portal/auth.setup.ts
@@ -5,7 +5,7 @@ import path from 'path';
 test('authenticate via GitHub and save state', async ({ page }) => {
   await page.goto('https://backstage.127-0-0-1.nip.io/');
 
-  await page.getByRole('button', { name: /github/i }).click();
+  await page.getByRole('listitem').filter({ hasText: 'GitHubSign in using' }).getByRole('button').click();
 
   await page.getByLabel('Username or email address').fill(process.env.E2E_TEST_GH_USERNAME!);
   await page.getByLabel('Password').fill(process.env.E2E_TEST_GH_PASSWORD!);
