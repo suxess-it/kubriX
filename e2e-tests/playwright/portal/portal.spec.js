70e866a530fc9f47d8afc5a9d003f89ff39eef00
Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>
2025-11-14 20:38:30 +0100

    Update portal.spec.js

--- e2e-tests/playwright/portal/portal.spec.js
+++ e2e-tests/playwright/portal/portal.spec.js
@@ -10,4 +10,10 @@ test("Smoke 1 - has title", async ({ page }) => {
   const popupPromise = page.waitForEvent('popup');
   await page.getByRole('listitem').filter({ hasText: 'GitHubSign in using' }).getByRole('button').click();
   const githubPage = await popupPromise;
+  // Optionally handle “Authorize” screen if it appears
+  const authorizeButton = githubPage.getByRole('button', { name: 'Authorize' });
+  if (await authorizeButton.isVisible()) {
+    await authorizeButton.click();
+  }
+  await githubPage.close();
 });
