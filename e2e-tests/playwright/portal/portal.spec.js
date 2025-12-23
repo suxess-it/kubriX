b03d8005f5f003a6549946e5ff5d6527ccc34b09
Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>
2025-12-23 23:03:23 +0100

    feat(e2e): extend tests with keycloak login

--- e2e-tests/playwright/portal/portal.spec.js
+++ e2e-tests/playwright/portal/portal.spec.js
@@ -1,7 +1,7 @@
 // @ts-check
 const { test, expect } = require("@playwright/test");
 
-test("Smoke 1 - has title", async ({ page }) => {
+test("Github Login", async ({ page }) => {
   await page.goto("https://backstage.127-0-0-1.nip.io/");
 
   await expect(page).toHaveTitle(/kubriX OSS/);
@@ -16,4 +16,30 @@ test("Smoke 1 - has title", async ({ page }) => {
   //  await authorizeButton.click();
   // }
   await githubPage.close();
+  await expect(page.getByRole('heading', { name: 'Welcome to kubriX' })).toBeVisible();
+  await page.getByTestId('sidebar-root').getByRole('link', { name: 'Settings' }).click();
+  await page.getByTestId('user-settings-menu').click();
+  await page.getByTestId('sign-out').click();
+});
+
+
+test('Keycloak Login', async ({ page }) => {
+  await page.goto("https://backstage.127-0-0-1.nip.io/");
+
+  await expect(page).toHaveTitle(/kubriX OSS/);
+
+  // Open Keycloak Login
+  const popupPromise = page.waitForEvent('popup');
+  await page.getByRole('listitem').filter({ hasText: 'Keycloak OIDCSign in with' }).getByRole('button').click();
+  const page1 = await popupPromise;
+  await page1.getByRole('textbox', { name: 'Username' }).click();
+  await page1.getByRole('textbox', { name: 'Username' }).fill('demoadmin');
+  await page1.getByRole('textbox', { name: 'Password' }).click();
+  await page1.getByRole('textbox', { name: 'Password' }).fill(process.env.E2E_KEYCLOAK_DEMOADMIN_PASSWORD!);
+  await page1.getByRole('button', { name: 'Sign In' }).click();
+  await page1.close();
+  await expect(page.getByRole('heading', { name: 'Welcome to kubriX' })).toBeVisible();
+  await page.getByTestId('sidebar-root').getByRole('link', { name: 'Settings' }).click();
+  await page.getByTestId('user-settings-menu').click();
+  await page.getByTestId('sign-out').click();
 });
