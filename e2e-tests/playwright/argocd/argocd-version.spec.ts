85ac0694a81c348c755c6ecefae920792667d2e6
Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>
2026-01-02 14:14:00 +0100

    Use environment variable for ArgoCD version check

--- e2e-tests/playwright/argocd/argocd-version.spec.ts
+++ e2e-tests/playwright/argocd/argocd-version.spec.ts
@@ -8,5 +8,5 @@ test.use({ storageState: argocdAuthFile });
 
 test('ArgoCD Version Info', async ({ page }) => {
   await page.goto('https://argocd.127-0-0-1.nip.io/api/version')
-  await expect(page.locator('pre')).toContainText('"Version":"v3.2.2+8d0dde1"');
+  await expect(page.locator('pre')).toContainText(process.env.E2E_ARGOCD_VERSION!);
 });
