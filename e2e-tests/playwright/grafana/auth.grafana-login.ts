012952cc44543bd410f21f2865e5a1e6ff3bc36a
Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>
2026-01-04 19:07:36 +0100

    Update Grafana login URL and wait for dashboard URL

--- e2e-tests/playwright/grafana/auth.grafana-login.ts
+++ e2e-tests/playwright/grafana/auth.grafana-login.ts
@@ -7,11 +7,11 @@ fs.mkdirSync(authDir, { recursive: true });
 const grafanaAuthFile = path.join(authDir, 'grafana.json');
 
 setup('authenticate', async ({ page }) => {
-  await page.goto('https://grafana.127-0-0-1.nip.io/');
+  await page.goto('https://grafana.127-0-0-1.nip.io/login');
   await page.locator('input[name="user"]').fill('admin');
   await page.locator('input[name="password"]').fill(process.env.E2E_GRAFANA_ADMIN_PASSWORD!);
   await page.locator('button[type="submit"]').click();
 
-  await page.waitForURL('https://grafana.127-0-0-1.nip.io/');
+  await page.waitForURL('https://grafana.127-0-0-1.nip.io/dashboards');
   await page.context().storageState({ path: grafanaAuthFile });
 });
