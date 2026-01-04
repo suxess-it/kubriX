d28b4bacaae79dae28e4a9a9fcbb347c6e0601c5
Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>
2026-01-04 18:59:00 +0100

    Update auth.grafana-login.ts

--- e2e-tests/playwright/grafana/auth.grafana-login.ts
+++ e2e-tests/playwright/grafana/auth.grafana-login.ts
@@ -7,13 +7,13 @@ fs.mkdirSync(authDir, { recursive: true });
 const grafanaAuthFile = path.join(authDir, 'grafana.json');
 
 setup('authenticate', async ({ page }) => {
-  await page.goto('https://grafana.127-0-0-1.nip.io/login?return_url=https%3A%2F%2Fargocd.127-0-0-1.nip.io%2Fapplications');
-  await page.getByRole('textbox', { name: 'Username' }).click();
-  await page.getByRole('textbox', { name: 'Username' }).fill('admin');
-  await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill(process.env.E2E_GRAFANA_ADMIN_PASSWORD!);
-  await page.getByRole('button', { name: 'Sign In' }).click();
+  await page.goto('https://grafana.127-0-0-1.nip.io/');
+  await page.getByRole('textbox', { name: 'user' }).click();
+  await page.getByRole('textbox', { name: 'user' }).fill('admin');
+  await page.getByRole('textbox', { name: 'password' }).click();
+  await page.getByRole('textbox', { name: 'password' }).fill(process.env.E2E_GRAFANA_ADMIN_PASSWORD!);
+  await page.getByRole('button', { name: 'submit' }).click();
 
-  await page.waitForURL('https://grafana.127-0-0-1.nip.io/applications');
+  await page.waitForURL('https://grafana.127-0-0-1.nip.io/');
   await page.context().storageState({ path: grafanaAuthFile });
 });
