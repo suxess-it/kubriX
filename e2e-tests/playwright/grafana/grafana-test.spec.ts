3a8d9cad6d0a9f305c09049644f36eaeae3c8676
Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>
2026-01-05 11:15:35 +0100

    test if grafana startup is seen in logs results
    Signed-off-by: Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>

--- e2e-tests/playwright/grafana/grafana-test.spec.ts
+++ e2e-tests/playwright/grafana/grafana-test.spec.ts
@@ -6,7 +6,13 @@ const authDir = path.join(__dirname, '../.auth');
 const grafanaAuthFile = path.join(authDir, 'grafana.json');
 test.use({ storageState: grafanaAuthFile });
 
-test('ArgoCD Version Info', async ({ page }) => {
-  await page.goto('https://argocd.127-0-0-1.nip.io/api/version')
-  await expect(page.locator('pre')).toContainText(process.env.E2E_ARGOCD_VERSION!);
+test('Grafana Loki Logs', async ({ page }) => {
+  await page.goto("https://grafana.127-0-0-1.nip.io/a/grafana-lokiexplore-app/explore/namespace/grafana/logs?from=now-3h&to=now&var-ds=loki&var-filters=namespace%7C%3D%7Cgrafana&patterns=%5B%5D&var-lineFormat=&var-fields=logger%7C%3D%7C__CV%CE%A9__%7B%22parser%22:%22logfmt%22__gfc__%22value%22:%22settings%22%7D,settings&var-levels=&var-metadata=&var-jsonFields=&var-patterns=&var-lineFilterV2=&var-lineFilters=caseInsensitive,0%7C__gfp__%3D%7CStarting%20Grafana&timezone=browser&var-all-fields=logger%7C%3D%7C__CV%CE%A9__%7B%22parser%22:%22logfmt%22__gfc__%22value%22:%22settings%22%7D,settings&displayedFields=%5B%5D&urlColumns=%5B%5D&visualizationType=%22logs%22&prettifyLogMessage=false&sortOrder=%22Descending%22&wrapLogMessage=false");
+
+  await expect
+  .poll(async () => {
+    const text = await page.getByText('Total:').textContent();
+    return Number(text?.match(/\d+/)?.[0]);
+  })
+  .toBeGreaterThan(0);
 });
