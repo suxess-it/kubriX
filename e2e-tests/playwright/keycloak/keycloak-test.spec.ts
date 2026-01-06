f7d167a1bb96b0d48c352fc125dcdd99f6ab7e61
Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>
2026-01-06 20:41:20 +0100

    fix(pipeline): e2e tests - search for keycloak clients (#1912)
    Signed-off-by: Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>

--- e2e-tests/playwright/keycloak/keycloak-test.spec.ts
+++ e2e-tests/playwright/keycloak/keycloak-test.spec.ts
@@ -9,6 +9,12 @@ test.use({ storageState: keycloakAuthFile });
 test('Keycloak check clients', async ({ page }) => {
   await page.goto("https://keycloak.127-0-0-1.nip.io/admin/master/console/#/kubrix/clients");
 
-  await expect(page.getByRole('link', { name: 'backstage', exact: true })).toBeVisible();
+  // we search for those clients instead of directly using "expect" because there can be multiple pages of clientsawait expect(page.getByRole('link', { name: 'vault', exact: true })).toBeVisible();
+  await page.getByRole('textbox', { name: 'Search' }).fill('vault');
+  await page.getByRole('button', { name: 'Search' }).click();
   await expect(page.getByRole('link', { name: 'vault', exact: true })).toBeVisible();
+
+  await page.getByRole('textbox', { name: 'Search' }).fill('backstage');
+  await page.getByRole('button', { name: 'Search' }).click();
+  await expect(page.getByRole('link', { name: 'backstage', exact: true })).toBeVisible();
 });
