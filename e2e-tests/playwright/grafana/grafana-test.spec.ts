eac7fe732bc2547967373dc00686d6e8997d8256
Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>
2026-01-07 09:33:27 +0100

    fix(pipeline): add grafana datasources check to e2e tests (#1913)
    Signed-off-by: Johannes Kleinlercher <johannes.kleinlercher@suxess-it.com>

--- e2e-tests/playwright/grafana/grafana-test.spec.ts
+++ e2e-tests/playwright/grafana/grafana-test.spec.ts
@@ -6,6 +6,15 @@ const authDir = path.join(__dirname, '../.auth');
 const grafanaAuthFile = path.join(authDir, 'grafana.json');
 test.use({ storageState: grafanaAuthFile });
 
+test('Grafana Check Datasources', async ({ page }) => {
+  test.slow();
+
+  await page.goto("https://grafana.127-0-0-1.nip.io/connections/datasources");
+
+  await expect(page.getByRole('link', { name: 'loki' , exact: true })).toBeVisible({ timeout: 20_000 });
+  await expect(page.getByRole('link', { name: 'mimir' , exact: true })).toBeVisible({ timeout: 20_000 });
+});
+
 test('Grafana Loki Logs', async ({ page }) => {
   test.slow();
   await page.goto("https://grafana.127-0-0-1.nip.io/a/grafana-lokiexplore-app/explore/namespace/grafana/logs?from=now-1h&to=now&var-ds=loki&var-filters=namespace%7C%3D%7Cgrafana&patterns=%5B%5D&var-lineFormat=&var-fields=logger%7C%3D%7C__CV%CE%A9__%7B%22parser%22:%22logfmt%22__gfc__%22value%22:%22settings%22%7D,settings&var-levels=&var-metadata=&var-jsonFields=&var-patterns=&var-lineFilterV2=&var-lineFilters=caseInsensitive,0%7C__gfp__%3D%7CStarting%20Grafana&timezone=browser&var-all-fields=logger%7C%3D%7C__CV%CE%A9__%7B%22parser%22:%22logfmt%22__gfc__%22value%22:%22settings%22%7D,settings&displayedFields=%5B%5D&urlColumns=%5B%5D&visualizationType=%22logs%22&prettifyLogMessage=false&sortOrder=%22Descending%22&wrapLogMessage=false");
@@ -14,7 +23,7 @@ test('Grafana Loki Logs', async ({ page }) => {
   .poll(async () => {
     const text = await page.getByText('Total:').textContent();
     return Number(text?.match(/\d+/)?.[0]);
-  })
+  }, { timeout: 20_000 })
   .toBeGreaterThan(0);
 });
 
