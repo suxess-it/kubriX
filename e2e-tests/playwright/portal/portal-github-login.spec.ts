69c56afc5bd2404b030f7539f14e123c10d35047
phac008 <philipp.achmueller@suxess-it.com>
2026-02-20 15:39:53 +0100

    remove fqdn from test
    Signed-off-by: phac008 <philipp.achmueller@suxess-it.com>

--- e2e-tests/playwright/portal/portal-github-login.spec.ts
+++ e2e-tests/playwright/portal/portal-github-login.spec.ts
@@ -291,7 +291,6 @@ test("Multi-Stage-Kargo App Onboarding", async ({ page }) => {
   await page.getByRole('textbox', { name: 'Name' }).fill(`a${prefix}-kubrixbot-app`);
   await page.getByRole('textbox', { name: 'Description' }).click();
   await page.getByRole('textbox', { name: 'Description' }).fill('this is a e2e test');
-  await page.getByRole('textbox', { name: 'FQDN' }).fill('127-0-0-1.nip.io');
   await page.getByRole('button', { name: 'Next' }).click();
   const page1Promise = page.waitForEvent('popup');
   await page.getByRole('button', { name: 'Log in' }).click();
