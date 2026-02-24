3ace8551567a50fb3417900fcb8423e7fb82d90c
phac008 <philipp.achmueller@suxess-it.com>
2026-02-24 16:46:58 +0100

    minor fixes
    Signed-off-by: phac008 <philipp.achmueller@suxess-it.com>

--- e2e-tests/playwright/vault/auth.vault-login.ts
+++ e2e-tests/playwright/vault/auth.vault-login.ts
@@ -8,8 +8,9 @@ const vaultAuthFile = path.join(authDir, 'vault.json');
 
 setup('authenticate', async ({ page }) => {
   await page.goto('https://vault.127-0-0-1.nip.io/');
-  await page.locator('[data-test-token]').fill(process.env.E2E_VAULT_ROOT_TOKEN!);
-  await page.locator('[data-test-auth-submit]').click();
+  await page.locator('input[name="token"]').waitFor();
+  await page.locator('input[name="token"]').fill(process.env.E2E_VAULT_ROOT_TOKEN!);
+  await page.locator('#auth-submit').click();
 
   await page.waitForURL('https://vault.127-0-0-1.nip.io/ui/vault/dashboard**');
   await page.context().storageState({ path: vaultAuthFile });
