f7a2d3ab2657956549e21978447d13c79f93f4c6
phac008 <philipp.achmueller@suxess-it.com>
2026-05-29 10:52:41 +0200

    chore(playwright): add oss/prime selector
    Signed-off-by: phac008 <philipp.achmueller@suxess-it.com>

--- e2e-tests/playwright/playwright.config.js
+++ e2e-tests/playwright/playwright.config.js
@@ -1,3 +1,14 @@
-const configModule = require('./playwright.oss.config.js');
+const fs = require('fs');
+const path = require('path');
+
+const profile = process.env.KUBRIX_PLAYWRIGHT_PROFILE ?? 'oss';
+const configFile = profile === 'prime'
+  ? './playwright.prime.config.js'
+  : './playwright.oss.config.js';
+const resolvedConfigFile = fs.existsSync(path.join(__dirname, configFile))
+  ? configFile
+  : './playwright.oss.config.js';
+
+const configModule = require(resolvedConfigFile);
 
 module.exports = configModule.default ?? configModule;
